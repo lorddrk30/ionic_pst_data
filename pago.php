ebef1af4c47af69f937871dec46bed47e424b572
erik <you@example.com>
2019-07-21 14:26:15 -0500

    post data en ionic

---
+++ pago.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "los datos enviados desde ionic por post son los siguientes post ";
+
+
+
+print_r($_POST);
+
+
+
+
+?>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private impulsse: InAppBrowser ) {}
+
+
+  Enviar(){
+
+
+
+    const browser = this.impulsse.create('http://localhost/enviopost/pago.php');
+    browser.executeScript({code: "(function() { alert(234);   var url = 'http://localhost/enviopost/pago.php';var data = {pago: '150',nombre:'erik'};fetch(url, {  method: 'POST',   body: JSON.stringify(data),   headers:{ 'Content-Type': 'application/json'  }}).then(res => res.json()).catch(error => console.error('Error:', error)).then(response => console.log('Success:', response));  })()"});
+  }
 
 }
